367c8cb0b118010bdc74032e41e155784f5f65b1
Suguru Inatomi <suguru.inatomi@gmail.com>
2024-06-05 17:50:28 +0900

    chore: add tailwindcss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,11 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styles: `
+    :host {
+      display: block;
+    }
+  `,
 })
 export class AppComponent {
   title = 'dog-ceo-viewer';
