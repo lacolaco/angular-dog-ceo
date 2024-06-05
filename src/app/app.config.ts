1466e43f410284a82c27743d34316aa0daa092d8
Suguru Inatomi <suguru.inatomi@gmail.com>
2024-06-05 17:15:42 +0900

    refactor: enable zoneless and event replay

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+  ],
 };
